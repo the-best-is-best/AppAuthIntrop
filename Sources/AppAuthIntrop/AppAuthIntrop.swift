bf426afff3014ecc0d1df02b7cc56df90ca166ac
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-10-27 17:45:04 +0300

    Fix formatting of OpenIdConfig usage in KAuthManager

--- Sources/AppAuthIntrop/AppAuthIntrop.swift
+++ Sources/AppAuthIntrop/AppAuthIntrop.swift
@@ -76,7 +76,7 @@ public class KAuthManager: NSObject {
             }
             
             Task {
-                let openId = await OpenIdConfig.shared
+                let openId =  OpenIdConfig.shared
                 
                 guard let redirectURI = URL(string: await openId.getRedirectUrl()) else {
                     await MainActor.run { completion(false, "Invalid redirect URL") }
